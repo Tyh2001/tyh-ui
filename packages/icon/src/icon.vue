ed82d37e263eac79e9f258a92d7e15bb73af0532
unknown <you@example.com>
2021-06-24 10:26:35 +0800

    更新 icon

---
+++ packages/icon/src/icon.vue
@@ -0,0 +1,53 @@
+<template>
+  <i :class="[iconClass]" :style="[iconStyle, sizeStyle]" />
+</template>
+
+<script>
+export default {
+  name: 'TyhIcon',
+  components: {},
+  props: {
+    // icon 的 Class
+    icon: String,
+    // icon 的颜色
+    color: {
+      type: String,
+      required: false,
+      default: '#606266'
+    },
+    // icon 的大小
+    size: {
+      type: String,
+      required: false,
+      default: '16'
+    }
+  },
+  data () {
+    return {}
+  },
+  computed: {
+    // icon 的 Class
+    iconClass () {
+      return `tyh-icon ${this.icon}`
+    },
+    // icon 的颜色
+    iconStyle () {
+      return {
+        color: this.color
+      }
+    },
+    // icon 的大小
+    sizeStyle () {
+      return {
+        fontSize: `${this.size}px`
+      }
+    }
+  },
+  watch: {},
+  created () { },
+  mounted () { },
+  methods: {}
+}
+</script>
+
+<style src="../style/index.css" scoped></style>

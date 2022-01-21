de9baf3423a2c3a1582b66eec38dc428509ff500
Tyh2001 <1469442737@qq.com>
2022-01-21 16:55:56 +0800

    change

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,5 @@
 <template>
-  <div>
-    <tyh-table :data="tableData" :columns="columns" />
-    <br />
-    <tyh-table :data="tableData" :columns="columns" trHeight="50px" />
-
-    <br />
-    <tyh-table :data="tableData" :columns="columns" align="center" />
-    <br />
-    <tyh-table
-      :data="tableData"
-      :columns="columns"
-      align="center"
-      border
-      zebra
-    />
-    <br />
-    <tyh-table
-      :data="tableData"
-      :columns="columns"
-      align="center"
-      width="500px"
-      height="200px"
-    />
-    <br />
-    <tyh-table :data="tableData" :columns="columns" align="center" num />
-    <br />
-    <tyh-table
-      :data="tableData"
-      :columns="columns"
-      align="center"
-      num
-      :important="[2, 4]"
-      importantColor="red"
-    />
-    <br />
-    <tyh-table :data="tableData" :columns="columns" align="center" />
-  </div>
+  <div></div>
 </template>
 
 <script>
@@ -44,55 +8,13 @@ export default {
   components: {},
   props: {},
   data () {
-    return {
-      columns: [
-        {
-          title: '日期',
-          key: 'date'
-        },
-        {
-          title: '姓名',
-          key: 'name'
-        },
-        {
-          title: '地址',
-          key: 'address'
-        }
-      ],
-      tableData: [
-        {
-          date: '2021-11-02',
-          name: '张三',
-          address: '浙江省杭州市上城区 231 号'
-        },
-        {
-          date: '2021-12-04',
-          name: '李四',
-          address: '浙江省杭州市西湖区 12 号'
-        },
-        {
-          date: '2021-10-01',
-          name: '王英',
-          address: '浙江省杭州市临平区 2 号'
-        },
-        {
-          date: '2021-11-03',
-          name: '李逵',
-          address: '浙江省杭州市拱墅区 199 号'
-        }
-      ]
-    }
+    return {}
   },
   computed: {},
   watch: {},
-  created () { },
-  mounted () { },
-  methods: {
-
-    aaa () {
-      console.log(12)
-    }
-  }
+  created () {},
+  mounted () {},
+  methods: {}
 }
 </script>
 
